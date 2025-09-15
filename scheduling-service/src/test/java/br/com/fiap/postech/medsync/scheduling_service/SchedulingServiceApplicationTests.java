bb38a298daab1f89046e3a6e07ecd7bef0b207ab
luizffdemoraes <lffm1994@gmail.com>
2025-09-14 22:51:42 -0300

    first commit

---
+++ scheduling-service/src/test/java/br/com/fiap/postech/medsync/scheduling_service/SchedulingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.medsync.scheduling_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
