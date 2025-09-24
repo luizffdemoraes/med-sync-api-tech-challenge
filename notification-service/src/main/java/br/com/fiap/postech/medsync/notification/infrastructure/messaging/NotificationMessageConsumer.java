dcf2e171b35c16fc95fb3dfda44fdddd5c01fd9a
luizffdemoraes <lffm1994@gmail.com>
2025-09-24 01:45:40 -0300

    Implementação de converão entre o objeto de dominio e objeto de consumer.

--- notification-service/src/main/java/br/com/fiap/postech/medsync/notification/infrastructure/messaging/NotificationMessageConsumer.java
+++ notification-service/src/main/java/br/com/fiap/postech/medsync/notification/infrastructure/messaging/NotificationMessageConsumer.java
@@ -3,6 +3,7 @@ package br.com.fiap.postech.medsync.notification.infrastructure.messaging;
 import br.com.fiap.postech.medsync.notification.application.usecases.CreateNotificationUseCase;
 import br.com.fiap.postech.medsync.notification.application.usecases.SendNotificationUseCase;
 import br.com.fiap.postech.medsync.notification.application.usecases.UpdateNotificationStatusUseCase;
+import br.com.fiap.postech.medsync.notification.domain.entities.NotificationStatus;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,12 +22,12 @@ public class NotificationMessageConsumer {
 
     @RabbitListener(queues = "${app.rabbitmq.queue}")
     public void receiveMessage(NotificationMessageDTO dto) {
-        var notification = createNotificationUseCase.create(dto); // status PENDING
+        var notification = createNotificationUseCase.create(dto.toDomain());
         try {
             sendNotificationUseCase.send(notification);
-            updateNotificationStatusUseCase.update(notification.getId(), "SENT");
+            updateNotificationStatusUseCase.updateStatus(notification.getId(), NotificationStatus.valueOf("SENT"));
         } catch(Exception e) {
-            updateNotificationStatusUseCase.update(notification.getId(), "FAILED");
+            updateNotificationStatusUseCase.updateStatus(notification.getId(), NotificationStatus.valueOf("FAILED"));
         }
     }
 }
---
+++ notification-service/src/main/java/br/com/fiap/postech/medsync/notification/infrastructure/messaging/NotificationMessageDTO.java
@@ -0,0 +1,74 @@
+package br.com.fiap.postech.medsync.notification.infrastructure.messaging;
+
+import java.time.LocalDateTime;
+
+public class NotificationMessageDTO {
+    private Long patientId;
+    private String patientEmail;
+    private LocalDateTime appointmentDate;
+    private String eventType;
+    private String message;
+
+    // Construtores
+    public NotificationMessageDTO() {
+    }
+
+    public NotificationMessageDTO(Long patientId, String patientEmail, LocalDateTime appointmentDate, String eventType, String message) {
+        this.patientId = patientId;
+        this.patientEmail = patientEmail;
+        this.appointmentDate = appointmentDate;
+        this.eventType = eventType;
+        this.message = message;
+    }
+
+    public Notification toDomain() {
+        return Notification.create(
+                this.patientId,
+                this.patientEmail,
+                this.appointmentDate,
+                this.eventType,
+                this.message
+        );
+    }
+
+    // Getters e Setters
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientEmail() {
+        return patientEmail;
+    }
+
+    public void setPatientEmail(String patientEmail) {
+        this.patientEmail = patientEmail;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
