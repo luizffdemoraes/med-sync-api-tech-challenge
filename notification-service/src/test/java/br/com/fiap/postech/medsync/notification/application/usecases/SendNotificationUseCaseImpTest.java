b291c5dfb09711e37708f4552f80f56aba71165c
luizffdemoraes <lffm1994@gmail.com>
2025-09-25 23:07:01 -0300

    Add unit test UseCaseImp.

--- notification-service/src/test/java/br/com/fiap/postech/medsync/notification/application/usecases/SendNotificationUseCaseImpTest.java
+++ notification-service/src/test/java/br/com/fiap/postech/medsync/notification/application/usecases/SendNotificationUseCaseImpTest.java
@@ -1,7 +1,43 @@
 package br.com.fiap.postech.medsync.notification.application.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.fiap.postech.medsync.notification.domain.entities.Notification;
+import br.com.fiap.postech.medsync.notification.domain.gateways.EmailNotificationGateway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.*;
 
 class SendNotificationUseCaseImpTest {
 
+    @InjectMocks
+    private SendNotificationUseCaseImp sendNotificationUseCaseImp;
+
+    @Mock
+    private EmailNotificationGateway emailNotificationGateway;
+
+
+    @BeforeEach
+    void setUp() {
+        emailNotificationGateway = mock(EmailNotificationGateway.class);
+        sendNotificationUseCaseImp = new SendNotificationUseCaseImp(emailNotificationGateway);
+    }
+
+    @Test
+    void shouldSendNotification() throws Exception {
+        Notification notification = Notification.create(
+                1L,
+                "lffm1994@gmail.com",
+                LocalDateTime.of(2025, 9, 28, 14, 0),
+                "MANUAL",
+                "Olá! Você tem uma consulta agendada para %s. %s"
+        );
+
+        sendNotificationUseCaseImp.send(notification);
+
+        verify(emailNotificationGateway, times(1)).send(notification);
+    }
 }
--- notification-service/src/test/java/br/com/fiap/postech/medsync/notification/application/usecases/UpdateNotificationStatusUseCaseImpTest.java
+++ notification-service/src/test/java/br/com/fiap/postech/medsync/notification/application/usecases/UpdateNotificationStatusUseCaseImpTest.java
@@ -1,7 +1,37 @@
 package br.com.fiap.postech.medsync.notification.application.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import br.com.fiap.postech.medsync.notification.domain.entities.NotificationStatus;
+import br.com.fiap.postech.medsync.notification.domain.gateways.NotificationGateway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
 
 class UpdateNotificationStatusUseCaseImpTest {
 
+    @InjectMocks
+    private UpdateNotificationStatusUseCaseImp updateNotificationStatusUseCaseImp;
+
+    @Mock
+    private NotificationGateway notificationGateway;
+
+
+    @BeforeEach
+    void setUp() {
+        notificationGateway = mock(NotificationGateway.class);
+        updateNotificationStatusUseCaseImp = new UpdateNotificationStatusUseCaseImp(notificationGateway);
+    }
+
+    @Test
+    void shouldUpdateNotificationStatus() {
+        Long notificationId = 1L;
+        NotificationStatus status = NotificationStatus.SENT;
+
+        updateNotificationStatusUseCaseImp.updateStatus(notificationId, status);
+
+        verify(notificationGateway, times(1)).updateStatus(notificationId, status);
+    }
 }
