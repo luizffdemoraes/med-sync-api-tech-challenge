9a9ef6b23fc43dc97dffb730e1bd665f5fed4756
luizffdemoraes <lffm1994@gmail.com>
2025-10-05 02:57:26 -0300

    Refactor

--- scheduling-service/src/main/java/br/com/fiap/postech/medsync/scheduling/infrastructure/config/dependency/DependencyInjectionConfig.java
+++ scheduling-service/src/main/java/br/com/fiap/postech/medsync/scheduling/infrastructure/config/dependency/DependencyInjectionConfig.java
@@ -10,8 +10,10 @@ import br.com.fiap.postech.medsync.scheduling.infrastructure.persistence.reposit
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
+@EnableSpringDataWebSupport
 public class DependencyInjectionConfig {
 
     // Controller
