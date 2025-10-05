2aed32723386c904e476907438316548bb91662a
luizffdemoraes <lffm1994@gmail.com>
2025-10-05 01:11:38 -0300

    Refactor

--- scheduling-service/src/main/java/br/com/fiap/postech/medsync/scheduling/application/dtos/AppointmentDTO.java
+++ scheduling-service/src/main/java/br/com/fiap/postech/medsync/scheduling/application/dtos/AppointmentDTO.java
@@ -3,8 +3,11 @@ package br.com.fiap.postech.medsync.scheduling.application.dtos;
 import br.com.fiap.postech.medsync.scheduling.domain.entities.Appointment;
 import br.com.fiap.postech.medsync.scheduling.domain.enums.AppointmentStatus;
 import br.com.fiap.postech.medsync.scheduling.domain.enums.AppointmentType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 
 public class AppointmentDTO {
 
