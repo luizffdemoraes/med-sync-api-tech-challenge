4c829c01268e722f0140b19dfbd64990fc50fcde
luizffdemoraes <lffm1994@gmail.com>
2025-10-07 03:29:39 -0300

    Refactor

--- history-service/src/main/java/br/com/fiap/postech/medsync/history/infrastructure/config/rabbitmq/RabbitMQConfig.java
+++ history-service/src/main/java/br/com/fiap/postech/medsync/history/infrastructure/config/rabbitmq/RabbitMQConfig.java
@@ -5,11 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class RabbitMQConfig {
@@ -37,13 +40,14 @@ public class RabbitMQConfig {
     @Bean
     public MessageConverter jsonMessageConverter() {
         ObjectMapper objectMapper = new ObjectMapper();
-
-        // Configurações básicas para ser tolerante com desserialização
-        objectMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
-        objectMapper.configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
-
-        // Para versões antigas, use o construtor diretamente
-        return new Jackson2JsonMessageConverter(objectMapper);
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(objectMapper);
+        converter.setClassMapper(new DefaultClassMapper() {
+            @Override
+            public Class<?> toClass(MessageProperties properties) {
+                return Map.class; // Sempre desserializa como Map
+            }
+        });
+        return converter;
     }
 
     // Bindings específicos para cada evento que queremos consumir
